54f9f031b6a826562d1670d364345d2bb1bf9325
Mr. Dockett <32434745+dockettj@users.noreply.github.com>
2022-01-31 13:06:15 -0500

    Starter Code Commit

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+#############################################
+#### Python Challenge 1.1 - Hello World #####
+#############################################
+
+# Create a program that displays "Hello World" in the console.
+
+# The goal of this challenge is to get you used to using GitHub Classroom.
+# The code that you need to complete this challenge is as follows:
+
+# print("Hello World")
+
+# Good luck!
